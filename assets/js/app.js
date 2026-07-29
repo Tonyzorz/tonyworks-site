64caaf69f904e81720d6e3747890ecaaa89c06e9
Taewon Yoon <tonyzorz@naver.com>
2026-07-29 23:20:27 +0900

    Add YouTube channel links

--- assets/js/app.js
+++ assets/js/app.js
@@ -301,6 +301,7 @@
         '<a href="/privacy-policy.html">Privacy</a> &#183; <a href="/terms.html">Terms</a> &#183; ' +
         '<a href="https://www.tiktok.com/@tonyworks95?lang=en" target="_blank" rel="noopener noreferrer">TikTok</a> &#183; ' +
         '<a href="https://www.instagram.com/tonytworks" target="_blank" rel="noopener noreferrer">Instagram</a> &#183; ' +
+        '<a href="https://www.youtube.com/@tonytworks" target="_blank" rel="noopener noreferrer">YouTube</a> &#183; ' +
         '<a href="mailto:tonyzorz@naver.com">tonyzorz@naver.com</a>';
     }
   }
